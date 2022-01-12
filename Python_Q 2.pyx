880c291b415367de9d8f83c97ccb762d0bc722b4
programmer3210 <97148310+programmer3210@users.noreply.github.com>
2022-01-12 12:29:49 +0530

    Add files via upload

---
+++ Python_Q 2.pyx
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 12 12:07:27 2022
+
+@author: Vidhi Singh
+"""
+
+list1=[12,-7,5,64,-14]
+print(list1)
+for i in list1:
+    if i<0:
+        list1.remove(i)
+        print(list1)
+        
+list2=[12,14,-95,3]
+print(list2)
+for e in list2:
+    if i<0:
+        list2.remove(e)
+        print(list2)
+        
+    
